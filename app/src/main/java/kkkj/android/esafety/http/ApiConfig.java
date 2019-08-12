f953ee5af8b4fd5c343bc1300f789e05b9cb05c2
ysyblue <790378968@qq.com>
2019-08-12 09:11:38 +0800

    修复bug

--- app/src/main/java/kkkj/android/esafety/http/ApiConfig.java
+++ app/src/main/java/kkkj/android/esafety/http/ApiConfig.java
@@ -1,6 +1,6 @@
 package kkkj.android.esafety.http;
 
 public class ApiConfig {
-    //基址
-    public final static String BASE_URL="http://quickcq.com:80/";
+    //基址http://quickcq.com:80/  http://acapi.bjjtza.com/
+    public final static String BASE_URL="http://acapi.bjjtza.com/";
 }
